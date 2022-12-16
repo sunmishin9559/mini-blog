44a854432ceb0741acbcc07377f3b7cb90ac689b
Sunmi.Shin <Sunmi.Shin@LAPTOP-CKGJ3M3>
2022-12-16 13:04:36 -0600

    mini-blog upload

---
+++ src/component/ui/Button.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyleButton = styled.button`
+    padding: 8px 16px;
+    font-size: 16px;
+    border-width: 1px;
+    border-radius: 8px;
+    curosr: pointer;
+`;
+
+function Button(props) {
+    const { title, onClick } = props;
+
+    return (
+        <StyleButton onClick={onClick}>
+            {title || "button"}
+        </StyleButton>
+    );
+}
+
+export default Button;
